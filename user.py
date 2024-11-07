5fbf3e9d8c9d482242bf440b20917be1ce7478fc
Dilmurodddd <mamarajabovdddd@gmail.com>
2024-11-07 17:27:58 +0500

    tajriba

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, firstname, lastname, age, job):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.job = job
+    
